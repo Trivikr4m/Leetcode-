568e3b0fa9fee5e4b63feaaa6d23f6523febee3f
Trivikrammmmm <trivikramsaisankeerth@gmail.com>
2026-09-02 20:03:06 +0530

    Time: 1 ms (99.97%) | Memory: 51.1 MB (42.89%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    public int atMost(int[] nums, int goal) {
+        if (goal < 0) {
+            return 0;
+        }
+
+        int l = 0;
+        int sum = 0;
+        int ans = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+
+            while (sum > goal) {
+                sum -= nums[l];
+                l++;
+            }
+
+            ans += r - l + 1;
+        }
+
+        return ans;
+    }
+}
