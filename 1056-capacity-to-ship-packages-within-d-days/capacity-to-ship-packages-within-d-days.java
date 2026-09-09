e2c63b3359f4581f4e1c8587f1347f2430f93104
Trivikrammmmm <trivikramsaisankeerth@gmail.com>
2026-09-09 08:18:56 +0530

    Time: 11 ms (64.53%) | Memory: 50.2 MB (37.58%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isCap(int[] n,int d,int m){
+        int c=0;
+        d--;
+        for(int i=0;i<n.length;i++){
+            c+=n[i];
+            if(c > m){
+                d--;
+                c = n[i];
+            }
+            if(d < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int l=0,r=0,mid;
+        for(int i=0;i<weights.length;i++){
+            r += weights[i] ;
+            l = Math.max(l,weights[i]);
+        }
+            
+            while(l<= r){
+                mid = l+(r-l)/2;
+                if(isCap(weights,days,mid)){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }
+        return l;
+    }
+}
