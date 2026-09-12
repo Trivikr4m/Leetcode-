6f7b95f541dc1d339ba8c2ecd79114c8ce362e50
Trivikrammmmm <trivikramsaisankeerth@gmail.com>
2026-09-12 11:39:32 +0530

    Time: 25 ms (62.65%) | Memory: 91.9 MB (82.77%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPossible(int[] candies,long k,int max){
+        long count = 0;
+        for(int i=0;i<candies.length;i++){
+            int val = candies[i];
+            count += val/max;
+            if(count >= k){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int maximumCandies(int[] candies, long k) {
+        int l=1;
+        int r=0;
+        int mid=0;
+
+        for(int x:candies){
+            r = Math.max(r,x);
+        }
+
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(isPossible(candies,k,mid)){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return r;
+    }
+}
