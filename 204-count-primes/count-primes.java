d9dcf3c1420817d7699b7a811b97055ac3c47e71
Trivikrammmmm <trivikramsaisankeerth@gmail.com>
2026-09-24 19:14:31 +0530

    Time: 621 ms (55.51%) | Memory: 79.7 MB (50.00%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int cnt = 0;
+
+        for(int i=2; i*i < n; i++){
+            if(!notPrime[i]){
+                for(int j=i*i; j < n; j+=i){
+                    notPrime[j] = true;
+                }
+            }
+        }
+
+        for(int i=2;i<n;i++){
+            if(!notPrime[i]){
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
